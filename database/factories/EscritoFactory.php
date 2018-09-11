c8f92ed464e0e13bdbe2b60edbe3585b0e8676a6
leagurru <leagurru@gmail.com>
2018-09-11 12:53:46 -0300

    diseño identico a proyecto cnat funcionando ok vista escrito.index funcionando ok todos los filtros
    seeder de mre ok completo

--- database/factories/EscritoFactory.php
+++ database/factories/EscritoFactory.php
@@ -5,10 +5,14 @@ use Faker\Generator as Faker;
 $factory->define(App\Modelos\User\Escrito::class, function (Faker $faker) {
     return [
         'user_id' => $faker->numberBetween(1,50),
-        'organismo_id' => $faker->numberBetween(1,4),
+        'organismo_id' => $faker->numberBetween(1,84),
+//        'organismo_id' => $faker->randomElement(
+//                                                1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,30,31,32,33,34,35,36,37,38,39,40,
+//                                                41,42,43,44,45,46,47,48,49,50,51,52,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,72,73,74,75,76,77,79,80,
+//                                                81,82,83,84
+//                            ),
         'fecha'       => $faker->date($format = 'Y-m-d', $max = 'now'), // '1979-06-09'
         'mre_id'      => $faker->randomElement([29,53]),
-//        'mre_id'      => $faker->randomElement([29,53,71,78]),
         'CausaNumero' => $faker->numberBetween(1,19999),
         'CausaAnio' => $faker->numberBetween(2016,2018),
         'caratula' => str_random(10),
--- database/seeds/MresSeeder.php
+++ database/seeds/MresSeeder.php
@@ -11,25 +11,436 @@ class MresSeeder extends Seeder
      */
     public function run()
     {
+        // **********************************
+        // MRE: Diagonal
+        // **********************************
         DB::table('mres')->insert([
             'mre_id' => '29',
             'organismo_id' => '1',
+            'ordenImpresion' => 26
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '2',
+            'ordenImpresion' => 27
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '3',
+            'ordenImpresion' => 11
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '4',
+            'ordenImpresion' => 8
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '5',
+            'ordenImpresion' => 9
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '6',
+            'ordenImpresion' => 10
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '7',
+            'ordenImpresion' => 23
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '8',
+            'ordenImpresion' => 28
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '9',
+            'ordenImpresion' => 5
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '10',
+            'ordenImpresion' => 6
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '11',
+            'ordenImpresion' => 12
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '12',
+            'ordenImpresion' => 13
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '13',
+            'ordenImpresion' => 7
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '14',
+            'ordenImpresion' => 2
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '15',
+            'ordenImpresion' => 3
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '16',
+            'ordenImpresion' => 4
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '17',
+            'ordenImpresion' => 1
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '18',
+            'ordenImpresion' => 24
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '19',
+            'ordenImpresion' => 25
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '20',
+            'ordenImpresion' => 20
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '21',
+            'ordenImpresion' => 21
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '22',
+            'ordenImpresion' => 22
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '23',
+            'ordenImpresion' => 17
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '24',
+            'ordenImpresion' => 18
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '25',
+            'ordenImpresion' => 19
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '26',
+            'ordenImpresion' => 14
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '27',
+            'ordenImpresion' => 15
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '29',
+            'organismo_id' => '28',
+            'ordenImpresion' => 16
+        ]);
+
+
+        // **********************************
+        // MRE: Lavalle
+        // **********************************
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '30',
+            'ordenImpresion' => 7
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '31',
             'ordenImpresion' => 1
         ]);
         DB::table('mres')->insert([
             'mre_id' => '53',
-            'organismo_id' => '2',
+            'organismo_id' => '32',
+            'ordenImpresion' => 2
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '33',
+            'ordenImpresion' => 3
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '34',
+            'ordenImpresion' => 4
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '35',
+            'ordenImpresion' => 5
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '36',
+            'ordenImpresion' => 6
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '37',
+            'ordenImpresion' => 15
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '38',
+            'ordenImpresion' => 16
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '39',
+            'ordenImpresion' => 17
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '40',
+            'ordenImpresion' => 18
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '41',
+            'ordenImpresion' => 11
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '42',
+            'ordenImpresion' => 12
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '43',
+            'ordenImpresion' => 13
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '44',
+            'ordenImpresion' => 14
+        ]);
+
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '45',
+            'ordenImpresion' => 19
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '46',
+            'ordenImpresion' => 20
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '47',
+            'ordenImpresion' => 21
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '48',
+            'ordenImpresion' => 22
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '49',
+            'ordenImpresion' => 23
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '50',
+            'ordenImpresion' => 8
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '51',
+            'ordenImpresion' => 9
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '53',
+            'organismo_id' => '52',
+            'ordenImpresion' => 10
+        ]);
+
+
+        // **********************************
+        // MRE: Perón
+        // **********************************
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '54',
+            'ordenImpresion' => 16
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '55',
+            'ordenImpresion' => 17
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '56',
+            'ordenImpresion' => 14
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '57',
+            'ordenImpresion' => 12
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '58',
+            'ordenImpresion' => 10
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '59',
+            'ordenImpresion' => 8
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '60',
+            'ordenImpresion' => 7
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '61',
+            'ordenImpresion' => 6
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '62',
+            'ordenImpresion' => 5
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '63',
+            'ordenImpresion' => 4
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '64',
+            'ordenImpresion' => 3
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '65',
+            'ordenImpresion' => 15
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '66',
+            'ordenImpresion' => 2
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '67',
+            'ordenImpresion' => 13
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '68',
+            'ordenImpresion' => 11
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '69',
             'ordenImpresion' => 1
         ]);
         DB::table('mres')->insert([
             'mre_id' => '71',
-            'organismo_id' => '30',
+            'organismo_id' => '70',
+            'ordenImpresion' => 9
+        ]);
+
+
+        // Juzgados de Cerrito, incorporado a la MRE de Perón
+
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '79',
+            'ordenImpresion' => 23
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '80',
+            'ordenImpresion' => 22
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '81',
+            'ordenImpresion' => 21
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '82',
+            'ordenImpresion' => 20
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '83',
+            'ordenImpresion' => 18
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '71',
+            'organismo_id' => '84',
+            'ordenImpresion' => 19
+        ]);
+
+
+
+
+
+        // **********************************
+        // MRE: Paraná
+        // **********************************
+        DB::table('mres')->insert([
+            'mre_id' => '78',
+            'organismo_id' => '72',
+            'ordenImpresion' => 3
+        ]);
+
+        DB::table('mres')->insert([
+            'mre_id' => '78',
+            'organismo_id' => '73',
+            'ordenImpresion' => 5
+        ]);
+
+        DB::table('mres')->insert([
+            'mre_id' => '78',
+            'organismo_id' => '74',
+            'ordenImpresion' => 4
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '78',
+            'organismo_id' => '75',
+            'ordenImpresion' => 2
+        ]);
+        DB::table('mres')->insert([
+            'mre_id' => '78',
+            'organismo_id' => '76',
             'ordenImpresion' => 1
         ]);
         DB::table('mres')->insert([
             'mre_id' => '78',
-            'organismo_id' => '31',
-            'ordenImpresion' => 1
+            'organismo_id' => '77',
+            'ordenImpresion' => 6
         ]);
+
+
+
     }
 }
